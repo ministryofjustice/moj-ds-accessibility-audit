2e0b6242ef96285eb9f4e3f575af2e851d688a75
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-03-25 12:17:45 +0000

    Add grant list pages

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,8 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/look-at-a-grant/switch-organisation', function (req, res) {
+  res.redirect('/look-at-a-grant/grants-' + req.body.organisation)
+})
+
 module.exports = router
