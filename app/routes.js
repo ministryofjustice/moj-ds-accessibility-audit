5452b1b42acd80fa1d74cd2a36e28d78585231aa
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-04-08 14:41:31 +0100

    Handle file uploads
    Add AJAX endpoints for JavaScript. For both AJAX and form submission, succeed on PDF uploads and fail on anything else.

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,31 @@ router.post('/look-at-a-grant/switch-organisation', function (req, res) {
   res.redirect('/look-at-a-grant/grants-' + req.body.organisation)
 })
 
+router.post('/ajax-upload', function (req, res) {
+  if (!req.files || req.files.documents.name.substr(-4) !== '.pdf') {
+    res.status(200).json({
+      error: { message: 'Invalid file type uploaded' },
+      file: { name: req.files.documents.name, filename: req.files.documents.name, originalname: req.files.documents.name }
+    })
+  }
+
+  res.status(200).json({
+    success: { messageHtml: 'File uploaded', messageText: 'File uploaded' },
+    file: { name: req.files.documents.name, filename: req.files.documents.name, originalname: req.files.documents.name }
+  })
+})
+
+router.post('/ajax-delete', function (req, res) {
+  res.status(200).json({})
+})
+
 router.post('/provide-some-documents/provide-some-documents', function (req, res) {
+  if (req.files) {
+    if (req.files.documents.name.substr(-4) !== '.pdf') {
+      res.redirect('/provide-some-documents/provide-some-documents-error')
+    }
+  }
+
   res.redirect('/provide-some-documents/success')
 })
 
