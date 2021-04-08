73fab00a7d14d680583250becb9ae93c592540cd
Beverley Newing <bnewing92@hotmail.co.uk>
2021-04-08 12:46:08 +0100

    Fix issue with form submission not working, and adds in success page

--- app/routes.js
+++ app/routes.js
@@ -7,4 +7,8 @@ router.post('/look-at-a-grant/switch-organisation', function (req, res) {
   res.redirect('/look-at-a-grant/grants-' + req.body.organisation)
 })
 
+router.post('/provide-some-documents/provide-some-documents', function (req, res) {
+  res.redirect('/provide-some-documents/success')
+})
+
 module.exports = router
